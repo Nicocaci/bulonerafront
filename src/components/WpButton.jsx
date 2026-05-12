a850bb2ee46f25bff0d44234ad689d8d393feac7
Nicolás Caci <nicko.caci@gmail.com>
2026-05-12 12:12:59 -0300

    wp

--- src/components/WpButton.jsx
+++ src/components/WpButton.jsx
@@ -3,7 +3,7 @@ import { FaWhatsapp } from "react-icons/fa";
 import "../css/WpButton.css";
 
 const WpButton = () => {
-  const phoneNumber = "+54 9 11 5154-4062";
+  const phoneNumber = "+5491151544062";
   const message = "Hola! Quiero hacer una consulta sobre sus productos";
 
   const url = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`;
