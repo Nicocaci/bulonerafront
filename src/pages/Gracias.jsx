a57da72fdf8a00e303f725bafa2dfb0636b538a8
Nicolás Caci <nicko.caci@gmail.com>
2026-09-23 17:48:12 -0300

    push

--- src/pages/Gracias.jsx
+++ src/pages/Gracias.jsx
@@ -16,8 +16,8 @@ const Gracias = () => {
         <div className="checkout-step-content">
           <h2>Tu pedido se ha realizado con éxito.</h2>
           <p>
-            En breve nos pondremos en contacto contigo para coordinar el envío y
-            los detalles de tu compra.
+            Hemos recibido tu pedido y estamos procesándolo. Te enviaremos un
+            correo electrónico de confirmación con los detalles de tu compra.
           </p>
           <div style={{ marginTop: "2rem" }}>
             <Link to="/productos" className="checkout-btn-primary">
